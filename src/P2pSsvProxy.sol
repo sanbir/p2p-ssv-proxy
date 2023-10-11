43424b7578eb7a87540039e53a5dfdd9b75583c9
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-10-11 11:50:40 +0300

    proxy all cluster functions

--- src/P2pSsvProxy.sol
+++ src/P2pSsvProxy.sol
@@ -3,91 +3,151 @@
 
 pragma solidity 0.8.18;
 
-import "./interfaces/IDepositContract.sol";
 import "./constants/P2pConstants.sol";
 import "./interfaces/ssv/ISSVClusters.sol";
+import "./@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./access/OwnableWithOperator.sol";
 
-contract P2pSsvProxy {
-    IDepositContract public immutable i_depositContract;
+/// @notice amount of parameters do no match
+error P2pSsvProxy__AmountOfParametersError();
 
+contract P2pSsvProxy is OwnableWithOperator {
     ISSVClusters public immutable i_ssvNetwork;
+    IERC20 public immutable i_ssvToken;
 
-    uint64 public immutable i_operatorId1;
-    uint64 public immutable i_operatorId2;
-    uint64 public immutable i_operatorId3;
-    uint64 public immutable i_operatorId4;
-
-    constructor(uint64[4] memory _operatorIds) {
-        i_depositContract = (block.chainid == 1) // Mainnet
-            ? IDepositContract(0x00000000219ab540356cBB839Cbe05303d7705Fa)
-            : (block.chainid == 5) // Goerli
-                ? IDepositContract(0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b)
-                : IDepositContract(0x4242424242424242424242424242424242424242); // Holesky
-
+    constructor() {
         i_ssvNetwork = (block.chainid == 1)
             ? ISSVClusters(0xDD9BC35aE942eF0cFa76930954a156B3fF30a4E1)
-            : (block.chainid == 5)
-                ? ISSVClusters(0xC3CD9A0aE89Fff83b71b58b6512D43F8a41f363D)
-                : ISSVClusters(address(0)); // TODO Holesky
+            : ISSVClusters(0xC3CD9A0aE89Fff83b71b58b6512D43F8a41f363D);
 
-        i_operatorId1 = _operatorIds[0];
-        i_operatorId2 = _operatorIds[1];
-        i_operatorId3 = _operatorIds[2];
-        i_operatorId4 = _operatorIds[3];
+        i_ssvToken = (block.chainid == 1)
+            ? IERC20(0x9D65fF81a3c488d585bBfb0Bfe3c7707c7917f54)
+            : IERC20(0x3a9f01091C446bdE031E39ea8354647AFef091E7);
+
+        i_ssvToken.approve(address(i_ssvNetwork), type(uint256).max);
     }
 
-    function getOperatorIds() private view returns(uint64[] memory) {
-        uint64[] memory operatorIds = new uint64[](4);
-        operatorIds[0] = i_operatorId1;
-        operatorIds[1] = i_operatorId2;
-        operatorIds[2] = i_operatorId3;
-        operatorIds[3] = i_operatorId4;
-
-        return operatorIds;
-    }
-
-    function depositAndRegisterOneValidator() private {
-        i_depositContract.deposit{value : COLLATERAL}(
-            _pubkeys[i],
-            withdrawalCredentials,
-            _signatures[i],
-            _depositDataRoots[i]
-        );
-
-        i_ssvNetwork.registerValidator(_pubkeys[i], operatorIds, sharesData[i], tokenPerValidator, cluster[i]);
-    }
-
-    function deposit(
+    function registerValidators(
         uint256 _tokenAmount,
-        address _eth2WithdrawalCredentialsAddress,
         bytes[] calldata _pubkeys,
-        bytes[] calldata _signatures,
-        bytes32[] calldata _depositDataRoots,
-        bytes[] calldata sharesData,
-        ISSVClusters.Cluster[] calldata cluster
-    ) external payable {
-        bytes memory withdrawalCredentials = abi.encodePacked(
-            hex'010000000000000000000000',
-            _eth2WithdrawalCredentialsAddress
-        );
+        uint64[] calldata _operatorIds,
+        bytes[] calldata _sharesData,
+        ISSVClusters.Cluster[] calldata _clusters
+    ) external onlyOperatorOrOwner {
+        uint256 validatorCount = _pubkeys.length;
 
-        uint64[] memory operatorIds = getOperatorIds();
+        if (!(
+            _sharesData.length == validatorCount &&
+            _clusters.length == validatorCount
+        )) {
+            revert P2pSsvProxy__AmountOfParametersError();
+        }
 
         uint256 tokenPerValidator = _tokenAmount / _pubkeys.length;
-//
-//        for (uint256 i = 0; i < validatorCount;) {
-//            i_depositContract.deposit{value : COLLATERAL}(
-//                _pubkeys[i],
-//                withdrawalCredentials,
-//                _signatures[i],
-//                _depositDataRoots[i]
-//            );
-//
-//            i_ssvNetwork.registerValidator(_pubkeys[i], operatorIds, sharesData[i], tokenPerValidator, cluster[i]);
-//
-//            unchecked {
-//                ++i;
-//            }
-//        }
+
+        for (uint256 i = 0; i < validatorCount;) {
+            i_ssvNetwork.registerValidator(_pubkeys[i], _operatorIds, _sharesData[i], tokenPerValidator, _clusters[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function removeValidators(
+        bytes[] calldata _pubkeys,
+        uint64[] calldata _operatorIds,
+        ISSVClusters.Cluster[] calldata _clusters
+    ) external onlyOperatorOrOwner {
+        uint256 validatorCount = _pubkeys.length;
+
+        if (!(
+            _clusters.length == validatorCount
+        )) {
+            revert P2pSsvProxy__AmountOfParametersError();
+        }
+
+        for (uint256 i = 0; i < validatorCount;) {
+            i_ssvNetwork.removeValidator(_pubkeys[i], _operatorIds, _clusters[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function liquidate(
+        uint64[] calldata _operatorIds,
+        ISSVClusters.Cluster[] calldata _clusters
+    ) external onlyOperatorOrOwner {
+        address clusterOwner = address(this);
+        uint256 validatorCount = _clusters.length;
+
+        for (uint256 i = 0; i < validatorCount;) {
+            i_ssvNetwork.liquidate(clusterOwner, _operatorIds, _clusters[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function reactivate(
+        uint256 _tokenAmount,
+        uint64[] calldata _operatorIds,
+        ISSVClusters.Cluster[] calldata _clusters
+    ) external onlyOperatorOrOwner {
+        uint256 tokenPerValidator = _tokenAmount / _clusters.length;
+        uint256 validatorCount = _clusters.length;
+
+        for (uint256 i = 0; i < validatorCount;) {
+            i_ssvNetwork.reactivate(_operatorIds, tokenPerValidator, _clusters[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function depositToSSV(
+        uint256 _tokenAmount,
+        uint64[] calldata _operatorIds,
+        ISSVClusters.Cluster[] calldata _clusters
+    ) external onlyOperatorOrOwner {
+        address clusterOwner = address(this);
+        uint256 tokenPerValidator = _tokenAmount / _clusters.length;
+        uint256 validatorCount = _clusters.length;
+
+        for (uint256 i = 0; i < validatorCount;) {
+            i_ssvNetwork.deposit(clusterOwner, _operatorIds, tokenPerValidator, _clusters[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function withdrawFromSSV(
+        uint256 _tokenAmount,
+        uint64[] calldata _operatorIds,
+        ISSVClusters.Cluster[] calldata _clusters
+    ) external onlyOperatorOrOwner {
+        uint256 tokenPerValidator = _tokenAmount / _clusters.length;
+        uint256 validatorCount = _clusters.length;
+
+        for (uint256 i = 0; i < validatorCount;) {
+            i_ssvNetwork.withdraw(_operatorIds, tokenPerValidator, _clusters[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function withdrawSSVTokens(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner {
+        i_ssvToken.transfer(_to, _amount);
     }
 }
