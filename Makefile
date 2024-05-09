6c87922b82edd1959e660ec3a4efd65e9b083433
Alexander Biryukov <alexander.biryukov@p2p.org>
2024-05-09 11:08:37 +0400

    test register

--- Makefile
+++ Makefile
@@ -10,7 +10,10 @@ snapshot :; forge snapshot
 
 format :; forge fmt
 
-NETWORK_ARGS := --rpc-url $(RPC_URL) --private-key $(PRIVATE_KEY) --broadcast --gas-price 27000000001 --priority-gas-price 100000001 --verify --etherscan-api-key $(ETHERSCAN_API_KEY) -vvvvv
+NETWORK_ARGS := --legacy --rpc-url $(RPC_URL) --private-key $(PRIVATE_KEY) --broadcast --gas-price 27000000001 --priority-gas-price 2000000001 --verify --etherscan-api-key $(ETHERSCAN_API_KEY) -vvvvv
 
 deploy:
 	@forge script script/Deploy.s.sol:Deploy $(NETWORK_ARGS)
+
+register:
+	@forge script script/Register.s.sol:Register $(NETWORK_ARGS)
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -13,8 +13,10 @@ contract Deploy is Script {
         P2pSsvProxyFactory,
         P2pSsvProxy
     ) {
-        address feeDistributorFactory = vm.envAddress("FEE_DISTRIBUTOR_FACTORY");
-        address referenceFeeDistributor = vm.envAddress("REFERENCE_FEE_DISTRIBUTOR");
+        IERC20 ssvToken = IERC20(0xad45A78180961079BFaeEe349704F411dfF947C6);
+
+        address feeDistributorFactory = 0x146847fA6Bee2b1b883dD2BD01B7242E7D6cfb8e;
+        address referenceFeeDistributor = 0xE4CdD87ea30B2Bea11067d4dcA4cDFC07819cA35;
 
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerKey);
@@ -23,10 +25,28 @@ contract Deploy is Script {
         P2pSsvProxy referenceP2pSsvProxy = new P2pSsvProxy(address(p2pSsvProxyFactory));
         p2pSsvProxyFactory.setReferenceP2pSsvProxy(address(referenceP2pSsvProxy));
 
+        ssvToken.transfer(address(p2pSsvProxyFactory), 50 ether);
+
+        address[] memory allowedSsvOperatorOwners = new address[](4);
+        allowedSsvOperatorOwners[0] = address(0x7bb11BE268088B000eD4B1b94014D40371A3a7d3);
+        allowedSsvOperatorOwners[1] = address(0x382f6FF5B9a29FcF1Dd2bf8B86C3234Dc7ed2Df6);
+        allowedSsvOperatorOwners[2] = address(0x262B0b1Dca4998BF8dbdF83b0cf63C2dC1d74F33);
+        allowedSsvOperatorOwners[3] = address(0xE5Bb5322A34e831a77bF06a25D13a0bE9B0b9B30);
+        p2pSsvProxyFactory.setAllowedSsvOperatorOwners(allowedSsvOperatorOwners);
+
         IChangeOperator(address(feeDistributorFactory)).changeOperator(address(p2pSsvProxyFactory));
+
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(326), 315, 0,0,0,0,0,0], allowedSsvOperatorOwners[0]);
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(236), 0,0,0,0,0,0,0], allowedSsvOperatorOwners[1]);
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(306), 0,0,0,0,0,0,0], allowedSsvOperatorOwners[2]);
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(400), 0,0,0,0,0,0,0], allowedSsvOperatorOwners[3]);
 
         p2pSsvProxyFactory.setSsvPerEthExchangeRateDividedByWei(uint112(vm.envUint("EXCHANGE_RATE")));
         p2pSsvProxyFactory.setMaxSsvTokenAmountPerValidator(uint112(vm.envUint("MAX_SSV_TOKEN_AMOUNT_PER_VALIDATOR")));
+
+        p2pSsvProxyFactory.changeOperator(vm.envAddress("FACTORY_OPERATOR"));
+
+        p2pSsvProxyFactory.transferOwnership(0x9aB843F2d60be2316F42B9764e98b532908AaB37);
 
         vm.stopBroadcast();
 
