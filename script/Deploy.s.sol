9837c1182e81cf5a3ccf12ca13ba9e6353585d6f
alexander.biryukov <alexander.biryukov@p2p.org>
2025-04-08 11:27:46 +0400

    set SSV operator IDs

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -9,35 +9,31 @@ import "../src/mocks/IChangeOperator.sol";
 
 contract Deploy is Script {
 
-    function run() external returns (
-        P2pSsvProxyFactory,
-        P2pSsvProxy
-    ) {
+    function run() external {
         address p2pOrgUnlimitedEthDepositor = vm.envAddress("P2P_ORG_UNLIMITED_ETH_DEPOSITOR");
         address feeDistributorFactory = vm.envAddress("FEE_DISTRIBUTOR_FACTORY");
         address referenceFeeDistributor = vm.envAddress("REFERENCE_FEE_DISTRIBUTOR");
 
+        address[] memory ssvOperatorOwners = new address[](4);
+        ssvOperatorOwners[0] = 0xfeC26f2bC35420b4fcA1203EcDf689a6e2310363;
+        ssvOperatorOwners[1] = 0x95b3D923060b7E6444d7C3F0FCb01e6F37F4c418;
+        ssvOperatorOwners[2] = 0x47659cc5fB8CDC58bD68fEB8C78A8e19549d39C5;
+        ssvOperatorOwners[3] = 0x9a792B1588882780Bed412796337E0909e51fAB7;
+
+        P2pSsvProxyFactory p2pSsvProxyFactory = P2pSsvProxyFactory(0x1f72FC2585D283DfEcF748cc5d19c014158A7C6f);
+
+
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerKey);
 
-        P2pSsvProxyFactory p2pSsvProxyFactory = new P2pSsvProxyFactory(
-            p2pOrgUnlimitedEthDepositor,
-            feeDistributorFactory,
-            referenceFeeDistributor
-        );
-        P2pSsvProxy referenceP2pSsvProxy = new P2pSsvProxy(address(p2pSsvProxyFactory));
-        p2pSsvProxyFactory.setReferenceP2pSsvProxy(address(referenceP2pSsvProxy));
 
-        // IChangeOperator(address(feeDistributorFactory)).changeOperator(address(p2pSsvProxyFactory));
+        p2pSsvProxyFactory.setAllowedSsvOperatorOwners(ssvOperatorOwners);
 
-        p2pSsvProxyFactory.setSsvPerEthExchangeRateDividedByWei(uint112(vm.envUint("EXCHANGE_RATE")));
-        p2pSsvProxyFactory.setMaxSsvTokenAmountPerValidator(uint112(vm.envUint("MAX_SSV_TOKEN_AMOUNT_PER_VALIDATOR")));
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(59),0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], ssvOperatorOwners[0]);
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(52),0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], ssvOperatorOwners[1]);
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(55),0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], ssvOperatorOwners[2]);
+        p2pSsvProxyFactory.setSsvOperatorIds([uint64(57),0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], ssvOperatorOwners[3]);
 
         vm.stopBroadcast();
-
-        return (
-            p2pSsvProxyFactory,
-            referenceP2pSsvProxy
-        );
     }
 }
