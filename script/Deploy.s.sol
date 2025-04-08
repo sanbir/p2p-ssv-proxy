60bbf60ce31cda5748509d9abb8591db57f43ad3
alexander.biryukov <alexander.biryukov@p2p.org>
2025-04-08 11:37:46 +0400

    set SSV operator IDs

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -9,23 +9,17 @@ import "../src/mocks/IChangeOperator.sol";
 
 contract Deploy is Script {
 
-    function run() external returns (
-        P2pSsvProxyFactory,
-        P2pSsvProxy
-    ) {
+    function run() external {
         IERC20 ssvToken = IERC20(vm.envAddress("SSV_TOKEN"));
 
         address feeDistributorFactory = vm.envAddress("FEE_DISTRIBUTOR_FACTORY");
         address referenceFeeDistributor = vm.envAddress("REFERENCE_FEE_DISTRIBUTOR");
 
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+
+        P2pSsvProxyFactory p2pSsvProxyFactory = P2pSsvProxyFactory(0xF4F9CC5383B68Cb982F4d56AC6EB4D97ba57496C);
+
         vm.startBroadcast(deployerKey);
-
-        P2pSsvProxyFactory p2pSsvProxyFactory = new P2pSsvProxyFactory(feeDistributorFactory, referenceFeeDistributor);
-        P2pSsvProxy referenceP2pSsvProxy = new P2pSsvProxy(address(p2pSsvProxyFactory));
-        p2pSsvProxyFactory.setReferenceP2pSsvProxy(address(referenceP2pSsvProxy));
-
-        ssvToken.transfer(address(p2pSsvProxyFactory), 50 ether);
 
         address[] memory allowedSsvOperatorOwners = new address[](4);
         allowedSsvOperatorOwners[0] = vm.envAddress("ALLOWED_OPERATOR_OWNER_1");
@@ -34,23 +28,12 @@ contract Deploy is Script {
         allowedSsvOperatorOwners[3] = vm.envAddress("ALLOWED_OPERATOR_OWNER_4");
         p2pSsvProxyFactory.setAllowedSsvOperatorOwners(allowedSsvOperatorOwners);
 
-        IChangeOperator(address(feeDistributorFactory)).changeOperator(address(p2pSsvProxyFactory));
-
         p2pSsvProxyFactory.setSsvOperatorIds([uint64(vm.envUint("OPERATOR_ID_1")), 0,0,0,0,0,0,0], allowedSsvOperatorOwners[0]);
         p2pSsvProxyFactory.setSsvOperatorIds([uint64(vm.envUint("OPERATOR_ID_2")), 0,0,0,0,0,0,0], allowedSsvOperatorOwners[1]);
         p2pSsvProxyFactory.setSsvOperatorIds([uint64(vm.envUint("OPERATOR_ID_3")), 0,0,0,0,0,0,0], allowedSsvOperatorOwners[2]);
         p2pSsvProxyFactory.setSsvOperatorIds([uint64(vm.envUint("OPERATOR_ID_4")), 0,0,0,0,0,0,0], allowedSsvOperatorOwners[3]);
 
-        p2pSsvProxyFactory.setSsvPerEthExchangeRateDividedByWei(uint112(vm.envUint("EXCHANGE_RATE")));
-        p2pSsvProxyFactory.setMaxSsvTokenAmountPerValidator(uint112(vm.envUint("MAX_SSV_TOKEN_AMOUNT_PER_VALIDATOR")));
-
-        p2pSsvProxyFactory.changeOperator(vm.envAddress("FACTORY_OPERATOR"));
 
         vm.stopBroadcast();
-
-        return (
-            p2pSsvProxyFactory,
-            referenceP2pSsvProxy
-        );
     }
 }
